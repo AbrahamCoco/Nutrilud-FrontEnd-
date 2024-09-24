73caf1737b62e15a1327c67042de0664fa7fcad2
AbrahamCoco <abraham_cryter2007@hotmail.com>
2024-09-24 15:59:38 -0600

    [Coco]Fixes responsive footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,15 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="footer py-4 bg-success bg-gradient text-light mt-auto">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
+      <Container>
+        <Row className="justify-content-center">
+          <Col md={4} sm={12} className="text-center text-md-start">
             <h5>Información de contacto:</h5>
             <ul className="list-unstyled">
               <li>
-                Telefono: <strong>246 265 3921</strong>
+                Teléfono: <strong>246 265 3921</strong>
               </li>
               <li>
                 Email: <strong>info@nutrilud.com o karinetza09@gmail.com</strong>
@@ -18,8 +19,8 @@ export default function Footer() {
                 Horario de consultas: <strong>Lunes a Viernes de 9:00 A.M. a 5:00 P.M.</strong>
               </li>
             </ul>
-          </div>
-          <div className="col-md-4 text-center">
+          </Col>
+          <Col md={4} sm={12} className="text-center">
             <h5>Redes sociales:</h5>
             <ul className="list-unstyled">
               <li>
@@ -41,8 +42,8 @@ export default function Footer() {
                 </a>
               </li>
             </ul>
-          </div>
-          <div className="col-md-4 text-end">
+          </Col>
+          <Col md={4} sm={12} className="text-center text-md-end">
             <h5>Recursos útiles:</h5>
             <ul className="list-unstyled">
               <li>
@@ -61,14 +62,15 @@ export default function Footer() {
                 </a>
               </li>
             </ul>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <p className="text-light">© {new Date().getFullYear()} NutriLud. Todos los derechos reservados.</p>
-          </div>
-        </div>
-      </div>
+          </Col>
+          <Col md={12} className="text-center">
+            NutriLud {new Date().getFullYear()} &copy; Todos los derechos reservados | Desarrollado por{" "}
+            <a href="https://github.com/DevConMx" className="text-light" target="_blank">
+              DevConMx
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
