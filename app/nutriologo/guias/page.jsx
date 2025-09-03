b418a5ef42bc3b348abbcf9f4d21699b99d63f5b
abrahamcryter2007 <abrahamcryter2007@gmail.com>
2025-09-03 21:37:00 +0000

    [Coco]Update firebase studio

--- app/nutriologo/guias/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function GuiasAlimentacion() {
-  return (
-    <div>
-      <h1>Guías de alimentación saludable</h1>
-      <p>En esta sección encontrarás guías de alimentación saludable para mejorar tu salud y bienestar.</p>
-    </div>
-  );
-}
--- app/nutriologo/recursos/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function RecursosNutricion() {
-  return (
-    <div>
-      <h1>Recursos de nutrición</h1>
-      <p>En esta sección encontrarás recursos útiles para mejorar tu alimentación y llevar un estilo de vida saludable.</p>
-    </div>
-  );
-}
